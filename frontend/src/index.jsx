64d5e3be58a35d914a611fea6828fece642839dc
meisun227 <meysunlee@gmail.com>
2023-08-06 13:53:44 +0300

    fetch and display forecasr frontend

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -3,37 +3,79 @@ import ReactDOM from 'react-dom';
 
 const baseURL = process.env.ENDPOINT;
 
-const getWeatherFromApi = async () => {
-  try {
-    const response = await fetch(`${baseURL}/weather`);
-    return response.json();
-  } catch (error) {
-    console.error(error);
-  }
-
-  return {};
-};
+// weather card display
+const WeatherCard = ({ forecastWeather }) => {
+  const weatherDetail = forecastWeather.weather.map((weather) => <div key={weather.id}><img src={`/img/${weather.icon.slice(0, -1)}.svg`}/><p>{weather.main}</p></div>)
+  return (
+    <div>
+      {weatherDetail}
+      <div className='cardContainer'>
+        <p>{forecastWeather.dt_txt}</p>
+        <p></p>
+      </div>
+    </div>
+  )
+}
 
 class Weather extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
       icon: "",
+      latitude: "",
+      longitude: "",
+      forecast: []
     };
   }
 
+  async getWeather() {
+    try {
+      const response = await fetch(`${baseURL}/weather?latitude=${this.state.latitude}&longitude=${this.state.longitude}`)
+      return response.json();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async getForecast() {
+    try {
+      const response = await fetch(`${baseURL}/forecast?latitude=${this.state.latitude}&longitude=${this.state.longitude}`)
+      return response.json();
+    }
+    catch (error) {
+      console.error(error);
+    }
+  }
+
+
   async componentDidMount() {
-    const weather = await getWeatherFromApi();
-    this.setState({icon: weather.icon.slice(0, -1)});
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.setState({ latitude: position.coords.latitude })
+      this.setState({ longitude: position.coords.longitude })
+      this.getWeather().then((weather) => {
+        this.setState({ icon: weather.icon.slice(0, -1) });
+      });
+      this.getForecast().then(response => {
+        this.setState({ forecast: response })
+      })
+    })
   }
 
   render() {
     const { icon } = this.state;
+    const forecastWeatherData = this.state.forecast
+    const weatherDisplay = forecastWeatherData.map((weather) => <WeatherCard key={weather.dt} forecastWeather={weather} />)
 
     return (
-      <div className="icon">
-        { icon && <img src={`/img/${icon}.svg`} /> }
+      <div className='container'>
+        <div className='top'>
+          <div className="icon">
+            {icon && <img src={`/img/${icon}.svg`} />}
+          </div>
+        </div>
+        <div className="weatherCard">
+          {weatherDisplay}
+        </div>
       </div>
     );
   }
