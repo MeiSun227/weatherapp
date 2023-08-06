9bf6d27cc13636852994aabf84095e64482a835b
meisun227 <meysunlee@gmail.com>
2023-08-06 17:39:47 +0300

    backend testing

--- backend/src/controller/weatherRouter.js
+++ backend/src/controller/weatherRouter.js
@@ -12,21 +12,24 @@ weatherRouter.get('/api/weather', async ctx => {
     const lat = ctx.query.latitude;
     const lon= ctx.query.longitude;
     const weatherData = await fetchWeather(lat,lon);
+
     ctx.type = 'application/json; charset=utf-8';
     ctx.body = weatherData.weather ? weatherData.weather[0] : {};
 });
 
-const fetchForecastWeather = async () => {
-    const endpoint = `${config.mapURI}/forecast?q=${config.targetCity}&appid=${config.appId}&`;
+const fetchForecastWeather = async (lat,lon) => {
+    const endpoint = `${config.mapURI}/forecast?lat=${lat}&lon=${lon}&cnt=4&appid=${config.appId}&`;
     const response = await fetch(endpoint);
     return response ? response.json() : {}
 };
 
 weatherRouter.get('/api/forecast', async ctx => {
-    const weatherData = await fetchForecastWeather();
+    const lat = ctx.query.latitude;
+    const lon= ctx.query.longitude;
+    const weatherData = await fetchForecastWeather(lat,lon);
 
     ctx.type = 'application/json; charset=utf-8';
-    ctx.body = weatherData.list ? weatherData.list : {};
+    ctx.body = weatherData?.list ? weatherData?.list : {};
 });
 
 module.exports = weatherRouter
