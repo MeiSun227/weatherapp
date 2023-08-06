ec6f79534bd8b4cdcdfd46ccd640ca5df896e1d8
meisun227 <meysunlee@gmail.com>
2023-08-06 10:38:40 +0300

    fetch api with geolocation

--- backend/src/controller/weatherRouter.js
+++ backend/src/controller/weatherRouter.js
@@ -2,16 +2,16 @@ const weatherRouter = require('koa-router')();
 const fetch = require('node-fetch');
 const config = require('../utils/config')
 
-const fetchWeather = async () => {
-    const endpoint = `${config.mapURI}/weather?q=${config.targetCity}&appid=${config.appId}&`;
+const fetchWeather = async (lat,lon) => {
+    const endpoint = `${config.mapURI}/weather?lat=${lat}&lon=${lon}&appid=${config.appId}`;
     const response = await fetch(endpoint);
-
     return response ? response.json() : {}
 };
 
 weatherRouter.get('/api/weather', async ctx => {
-    const weatherData = await fetchWeather();
-
+    const lat = ctx.query.latitude;
+    const lon= ctx.query.longitude;
+    const weatherData = await fetchWeather(lat,lon);
     ctx.type = 'application/json; charset=utf-8';
     ctx.body = weatherData.weather ? weatherData.weather[0] : {};
 });
@@ -19,7 +19,6 @@ weatherRouter.get('/api/weather', async ctx => {
 const fetchForecastWeather = async () => {
     const endpoint = `${config.mapURI}/forecast?q=${config.targetCity}&appid=${config.appId}&`;
     const response = await fetch(endpoint);
-
     return response ? response.json() : {}
 };
 
--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,6 @@ const config = require('./utils/config')
 const app = require('./app')
 const debug = require('debug')('weathermap');
 
-
 app.listen(config.PORT, () => {
-  console.log(`Server running on port ${config.PORT}`)
+  console.log(`Server running on port ${config.PORT}`)  
 })
