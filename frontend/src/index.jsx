4b2657a6c3a726e71704c4bbc486d06a4c25a483
meisun227 <meysunlee@gmail.com>
2023-08-06 20:04:06 +0300

    fix eslint errors

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,76 +1,92 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 
 const baseURL = process.env.ENDPOINT;
 
 // weather card display
 const WeatherCard = ({ forecastWeather }) => {
-  const weatherDetail = forecastWeather.weather.map((weather) => <div key={weather.id}><img src={`/img/${weather.icon.slice(0, -1)}.svg`}/><p>{weather.main}</p></div>)
+  const weatherDetail = forecastWeather.weather.map((weather) => (
+    <div key={weather.id}>
+      <img src={`/img/${weather.icon.slice(0, -1)}.svg`} alt="weatherIcon" />
+      <p>
+        {weather.main}
+      </p>
+    </div>
+  ));
+
   return (
     <div>
       {weatherDetail}
-      <div className='cardContainer'>
-        <p>{forecastWeather.dt_txt}</p>
-        <p></p>
+      <div className="cardContainer">
+        <p>
+          {forecastWeather.dt_txt}
+        </p>
       </div>
     </div>
-  )
-}
+  );
+};
+
+WeatherCard.propTypes = {
+  forecastWeather: PropTypes.element.isRequired,
+};
 
 class Weather extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      icon: "",
-      latitude: "",
-      longitude: "",
-      forecast: []
+      icon: '',
+      latitude: '',
+      longitude: '',
+      forecastWeatherData: [],
     };
   }
 
+  async componentDidMount() {
+    global.navigator.geolocation.getCurrentPosition((position) => {
+      this.setState({ latitude: position.coords.latitude });
+      this.setState({ longitude: position.coords.longitude });
+      this.getWeather().then((weather) => {
+        this.setState({ icon: weather.icon.slice(0, -1) });
+      });
+      this.getForecast().then((response) => {
+        this.setState({ forecastWeatherData: response });
+      });
+    });
+  }
+
   async getWeather() {
+    const { latitude, longitude } = this.state;
     try {
-      const response = await fetch(`${baseURL}/weather?latitude=${this.state.latitude}&longitude=${this.state.longitude}`)
+      const response = await fetch(`${baseURL}/weather?latitude=${latitude}&longitude=${longitude}`);
       return response.json();
     } catch (error) {
       console.error(error);
+      return {};
     }
   }
 
   async getForecast() {
+    const { latitude, longitude } = this.state;
     try {
-      const response = await fetch(`${baseURL}/forecast?latitude=${this.state.latitude}&longitude=${this.state.longitude}`)
+      const response = await fetch(`${baseURL}/forecast?latitude=${latitude}&longitude=${longitude}`);
       return response.json();
-    }
-    catch (error) {
+    } catch (error) {
       console.error(error);
+      return {};
     }
-  }
-
-
-  async componentDidMount() {
-    navigator.geolocation.getCurrentPosition((position) => {
-      this.setState({ latitude: position.coords.latitude })
-      this.setState({ longitude: position.coords.longitude })
-      this.getWeather().then((weather) => {
-        this.setState({ icon: weather.icon.slice(0, -1) });
-      });
-      this.getForecast().then(response => {
-        this.setState({ forecast: response })
-      })
-    })
   }
 
   render() {
-    const { icon } = this.state;
-    const forecastWeatherData = this.state.forecast
-    const weatherDisplay = forecastWeatherData.map((weather) => <WeatherCard key={weather.dt} forecastWeather={weather} />)
+    const { icon, forecastWeatherData } = this.state;
+    const weatherDisplay = forecastWeatherData
+      .map((weather) => <WeatherCard key={weather.dt} forecastWeather={weather} />);
 
     return (
-      <div className='container'>
-        <div className='top'>
+      <div className="container">
+        <div className="top">
           <div className="icon">
-            {icon && <img src={`/img/${icon}.svg`} />}
+            {icon && <img src={`/img/${icon}.svg`} alt="weatherIcon" />}
           </div>
         </div>
         <div className="weatherCard">
@@ -83,5 +99,5 @@ class Weather extends React.Component {
 
 ReactDOM.render(
   <Weather />,
-  document.getElementById('app')
+  document.getElementById('app'),
 );
